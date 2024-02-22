599ea998650ce078724d8c196f475273305e8e6c
BlackSiao <76524973+BlackSiao@users.noreply.github.com>
2024-02-22 11:53:25 +0800

    Add files via upload
    找到解决方法了，尝试自己把langchain的版本降级为：0.0.314
    // pip install langchain==0.0.314

--- file_deal.py
+++ file_deal.py
@@ -1,11 +1,16 @@
-from langchain_community.document_loaders import DirectoryLoader
+from langchain.document_loaders import DirectoryLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 
-def load_documents(directory="book"):
+def load_documents(directory="./book"):
     loader = DirectoryLoader(directory, show_progress=True)
     documents = loader.load()
-    for document in documents:
-        print(document)
+    text_spliter = CharacterTextSplitter(chunk_size=256, chunk_overlap=0)
+    docs_spliter = text_spliter.split_documents(documents)
+
+    print(docs_spliter[:2])
+    return docs_spliter
 
 
 load_documents()
+
